0e368c23ebed009ea7fd65d2930f141dd57b6b7f
Yuka Yanai <yukayanai@gmail.com>
2020-07-03 08:44:34 +1000

    Revert "rearranged files and started work on the dialog"

--- docs/style.a6dae8f7.js
+++ docs/style.a6dae8f7.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\img\\Plumtree-hero-image.jpg":[["Plumtree-hero-image.350f378d.jpg","img/Plumtree-hero-image.jpg"],"img/Plumtree-hero-image.jpg"],"./..\\img\\Plumtree-bg-image.jpg":[["Plumtree-bg-image.accd007a.jpg","img/Plumtree-bg-image.jpg"],"img/Plumtree-bg-image.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../img/Plumtree-hero-image.jpg":[["Plumtree-hero-image.350f378d.jpg","img/Plumtree-hero-image.jpg"],"img/Plumtree-hero-image.jpg"],"./../img/Plumtree-bg-image.jpg":[["Plumtree-bg-image.accd007a.jpg","img/Plumtree-bg-image.jpg"],"img/Plumtree-bg-image.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55923" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49800" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,3 @@
 console.log('parcel works')
 
 import '@fortawesome/fontawesome-free/css/all.css'
-
-document.addEventListener('DOMContentLoaded', function (event) {
-	const $ = (selector, $parent = document) => $parent.querySelector(selector)
-	const $$ = (selector, $parent = document) =>
-		Array.prototype.slice.call($parent.querySelectorAll(selector))
-
-	const $hamburger = $('#hamburger')
-	const $navDialog = $('#nav-dialog')
-
-	$hamburger.addEventListener('click', (e) => {
-		$navDialog.classList.add('open')
-	})
-})
